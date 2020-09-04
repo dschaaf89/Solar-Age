f225d5dfe9267bcc53b1d82cd304ead4a4120296
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-09-04 13:12:26 -0700

    it will add to the results tab the life expectancy left on Mercury

--- src/main.js
+++ src/main.js
@@ -3,22 +3,28 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Calculator from './js/calculator-logic';
-function displayResults(x){
-  $("#name").text(x.name);
-  $("#mercury").text(x.ageOnMercury(x.age)+" years old");
-  $("#venus").text(x.ageOnVenus(x.age)+" years old");
-  $("#mars").text(x.ageOnMars(x.age)+" years old");
-  $("#jupiter").text(x.ageOnJupiter(x.age)+" years old");
+function displayResults(x) {
+  if(x.age < 72){
+    $("#personName").text(x.name);
+    $("#mercury").text(x.ageOnMercury(x.age) + " years old");
+    $("#mercuryLife").text(x.lifeExpectancyMercury(x.age)+" years left in your life.");
+    $("#venus").text(x.ageOnVenus(x.age) + " years old");
+    $("#mars").text(x.ageOnMars(x.age) + " years old");
+    $("#jupiter").text(x.ageOnJupiter(x.age) + " years old");
+  }
+  
+
+
 }
 $(document).ready(function () {
   $("#person").submit(function (event) {
     event.preventDefault();
     let person = $('#name').val();
     let age = $('#age').val();
-    console.log(person,age);
-    let calc = new Calculator(person,age);
+    
+    let calc = new Calculator(person, age);
     $('#results').show();
     displayResults(calc);
-    
+
   });
 });
