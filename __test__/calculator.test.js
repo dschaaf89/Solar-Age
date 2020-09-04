c7cd578eca7f88e0f716dc12cfc5921d43c27a47
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-09-04 08:38:45 -0700

    it will make the first test on calculator.test.js which will fail becuase there is no code for it to test yet forgot to save before last commit

---
+++ __test__/calculator.test.js
@@ -0,0 +1,12 @@
+import { Calculator } from '../src/js/calculator-logic-logic.js';
+
+describe('Calculator', () => {
+  let person;
+
+  beforeEach(() => {
+    person = new Calculator("steve,18")
+  });
+  test('should create a level one character', () => {
+    expect('should create calculator object with name and age.')
+  });
+});
