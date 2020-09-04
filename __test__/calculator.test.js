609329d91751c69c28b7951cffad9780a6f12caf
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-09-04 09:57:43 -0700

    it will fix text in my text of mars to say mars and not venus

--- __test__/calculator.test.js
+++ __test__/calculator.test.js
@@ -27,7 +27,7 @@ test("should calculate how many more years a person who is 18 has left on mercur
 test("should calculate how many more years a person who is 18 has left on Venus", () => {
   expect(person.lifeExpectancyVenus(person.age)).toEqual(87);
 });
-test("should calculate how many more years a person who is 18 has left on Venus", () => {
+test("should calculate how many more years a person who is 18 has left on Mars", () => {
   expect(person.lifeExpectancyMars(person.age)).toEqual(28);
 });
 });
