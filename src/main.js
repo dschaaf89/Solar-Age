b98521d8170900f18d52627ff9d09ed75fa4f588
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-09-04 12:50:35 -0700

    it will start the code for the results tab for the calculator

--- src/main.js
+++ src/main.js
@@ -2,13 +2,15 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+//import Calculator from './js/calculator-logic';
 
 $(document).ready(function () {
   $("#person").submit(function (event) {
     event.preventDefault();
-    let name = $('#name').val();
+    let person = $('#name').val();
     let age = $('#age').val();
-    console.log(name);
-    console.log(age);
+    console.log(person,age);
+    //let calc = new Calculator(person,age);
+    
   });
 });
