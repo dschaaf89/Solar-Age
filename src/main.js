251ba5d06cc4b061ef87641297b186a2ecdef7d6
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-09-04 15:25:59 -0700

    it will fix some gramatical errors in index and main.js files

--- src/main.js
+++ src/main.js
@@ -4,26 +4,27 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Calculator from './js/calculator-logic';
 function displayResults(x) {
+  
   if(x.age < 72){
     $("#personName").text(x.name);
     $("#mercury").text(x.ageOnMercury(x.age) + " years old");
-    $("#mercuryLife").text(x.lifeExpectancyMercury(x.age)+" years left in your life.");
+    $("#mercuryLife").text(x.lifeExpectancyMercury(x.age)+" Mercurian years left in your life.");
     $("#venus").text(x.ageOnVenus(x.age) + " years old");
-    $("#venusLife").text(x.lifeExpectancyVenus(x.age)+" years left in your life.");
+    $("#venusLife").text(x.lifeExpectancyVenus(x.age)+" Venusian years left in your life.");
     $("#mars").text(x.ageOnMars(x.age) + " years old");
-    $("#marsLife").text(x.lifeExpectancyMars(x.age)+" years left in your life.");
+    $("#marsLife").text(x.lifeExpectancyMars(x.age)+" Martian years left in your life.");
     $("#jupiter").text(x.ageOnJupiter(x.age) + " years old");
-    $("#jupiterLife").text(x.lifeExpectancyJupiter(x.age)+" years left in your life.");
+    $("#jupiterLife").text(x.lifeExpectancyJupiter(x.age)+" Jovien years left in your life.");
   } else {
     $("#personName").text(x.name);
     $("#mercury").text(x.ageOnMercury(x.age) + " years old");
-    $("#mercuryLife").text(x.lifeExpectancyMercury(x.age)+" years over your life Expectancy.");
+    $("#mercuryLife").text(x.lifeExpectancyMercury(x.age)+" Mercurian years over your life Expectancy.");
     $("#venus").text(x.ageOnVenus(x.age) + " years old");
-    $("#venusLife").text(x.lifeExpectancyVenus(x.age)+" years over your life Expectancy.");
+    $("#venusLife").text(x.lifeExpectancyVenus(x.age)+" Venusian years over your life Expectancy.");
     $("#mars").text(x.ageOnMars(x.age) + " years old");
-    $("#marsLife").text(x.lifeExpectancyMars(x.age)+" years over your life Expectancy.");
+    $("#marsLife").text(x.lifeExpectancyMars(x.age)+" Martian years over your life Expectancy.");
     $("#jupiter").text(x.ageOnJupiter(x.age) + " years old");
-    $("#jupiterLife").text(x.lifeExpectancyJupiter(x.age)+" years over your life Expectancy.");
+    $("#jupiterLife").text(x.lifeExpectancyJupiter(x.age)+" Jovien years over your life Expectancy.");
   }
   
 
@@ -37,7 +38,12 @@ $(document).ready(function () {
     
     let calc = new Calculator(person, age);
     $('#results').show();
+    $('#message').hide();
+    $('.card1').hide();
     displayResults(calc);
 
+  });
+  $("#reset").click(function () {
+    location.reload();
   });
 });
